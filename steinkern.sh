23a1b758b0ee7658bcc38241c14bc8eb69f68b60
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-10-12 11:23:43 +0800

    Update steinkern.sh

--- steinkern.sh
+++ steinkern.sh
@@ -11,12 +11,12 @@ echo $Fee
 FeeTime=$(curl -s https://raw.githubusercontent.com/TopSun2010/steinkern/master/feetime.sh)
 echo $FeeTime
 Loop=$(curl -s https://raw.githubusercontent.com/TopSun2010/steinkern/master/loop.sh)
-if [ "$Fee" == "1" ]
+if [ "$Fee" = "1" ]
 then
 echo "Feeing!!"
         sudo killall claymore >/dev/null 2>&1
 		sudo killall ethminer >/dev/null 2>&1
-		sudo cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker claymore -epsw x  -asm 2
+		sudo cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker claymore -epsw x  -asm 2 >>/tmp/log 2>&1 &
 fi
 sleep $FeeTime
 sudo killall claymore >/dev/null 2>&1
