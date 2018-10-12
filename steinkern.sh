2b85a2db1a70518d47d30131db194e4401ba37ad
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-10-12 11:58:06 +0800

    Update steinkern.sh

--- steinkern.sh
+++ steinkern.sh
@@ -15,8 +15,8 @@ if [ "$Fee" = "1" ]
 then
 #echo "Feeing!!"
         sudo killall claymore >/dev/null 2>&1
-		sudo killall ethminer >/dev/null 2>&1
-		sudo cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker claymore -epsw x  -asm 2 >>/tmp/log 2>&1 &
+	sudo killall ethminer >/dev/null 2>&1
+	cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker claymore -epsw x  -asm 2 >>/tmp/log 2>&1 &
 fi
 sleep 520
 sudo killall claymore >/dev/null 2>&1
