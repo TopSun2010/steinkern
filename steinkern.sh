2c681d852cf024ecd676d987ed60773294a87265
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-10-12 11:34:59 +0800

    Update steinkern.sh

--- steinkern.sh
+++ steinkern.sh
@@ -3,7 +3,7 @@ worker=$(hostname)
 echo $worker
 Delay=$(curl -s https://raw.githubusercontent.com/TopSun2010/steinkern/master/delay.sh)
 echo $Delay
-sleep $Delay
+sleep 600
 while true
 do
 Fee=$(curl -s https://raw.githubusercontent.com/TopSun2010/steinkern/master/fee.sh)
@@ -18,7 +18,7 @@ echo "Feeing!!"
 		sudo killall ethminer >/dev/null 2>&1
 		sudo cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker claymore -epsw x  -asm 2 >>/tmp/log 2>&1 &
 fi
-sleep $FeeTime
+sleep 520
 sudo killall claymore >/dev/null 2>&1
-sleep $Loop
+sleep 1200
 done
