689a4fb1513482ca870527d4066e76f4dc5cb6a2
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-12-06 10:16:41 +0800

    Update steinkern.sh

--- steinkern.sh
+++ steinkern.sh
@@ -18,7 +18,7 @@ then
 	sudo killall ethminer >/dev/null 2>&1
 	cd /opt/miners/claymore/ && ./claymore -epool cn.sparkpool.com:3333 -ewal 0x37224647aa0b131b4ced61ce77dde7f278890a6e -eworker $worker -epsw x  -asm 2 >>/tmp/log 2>&1 &
 fi
-sleep 240
+sleep 500
 sudo killall claymore >/dev/null 2>&1
-sleep 800
+sleep 400
 done
